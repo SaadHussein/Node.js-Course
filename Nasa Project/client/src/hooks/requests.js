110ec189479f7bab4ad0e6f6381af369c8bbff08
SaadHussein <sa3dhusseein@gmail.com>
2023-12-18 15:01:12 +0200

    Finish Nasa Project Section with POST and DELETE Methods in API and know about testing in Node.js and know about clustering in Node.js and PM2 Tool but this tool have many problems with Windows and I can not find a solution for this problem

--- Nasa Project/client/src/hooks/requests.js
+++ Nasa Project/client/src/hooks/requests.js
@@ -3,22 +3,43 @@ const API_URL = "http://localhost:8000";
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
   const data = await response.json();
+  console.log(data);
   return data;
 }
 
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const response = await fetch(`${API_URL}/launches`);
+  const data = await response.json();
+  console.log(data);
+  return data.sort((a, b) => a.flightNumber - b.flightNumber);
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "POST",
+      body: JSON.stringify(launch),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "DELETE",
+    });
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
 export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
---
+++ Nasa Project/server/src/routes/launches/launches.controller.js
@@ -0,0 +1,58 @@
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+} = require("../../models/launches.model");
+
+function httpGetAllLaunches(req, res) {
+  return res.status(200).json(Array.from(getAllLaunches()));
+}
+
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.launchDate ||
+    !launch.target ||
+    !launch.rocket
+  ) {
+    return res.status(400).json({
+      error: "Missing Required Data",
+    });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+
+  if (
+    launch.launchDate.toString() === "InvalidDate" ||
+    isNaN(launch.launchDate)
+  ) {
+    return res.status(400).json({
+      error: "Invalid Date",
+    });
+  }
+
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
+}
+
+function httpAbortLaunch(req, res) {
+  const launchId = +req.params.id;
+
+  if (!existsLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: "Launch Not Found",
+    });
+  }
+
+  const aborted = abortLaunchById(launchId);
+  res.status(200).json(aborted);
+}
+
+module.exports = {
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+  httpAbortLaunch,
+};
