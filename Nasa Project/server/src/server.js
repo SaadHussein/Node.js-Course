736c59851f2347a38e9e126e8ea8ee280154a737
SaadHussein <sa3dhusseein@gmail.com>
2023-12-29 14:29:43 +0200

    Learning about Github Actions and CI/CD and Docker and AWS Deployment

--- Nasa Project/server/src/server.js
+++ Nasa Project/server/src/server.js
@@ -12,7 +12,7 @@ async function startServer() {
   await loadPlanets();
   await loadLaunchData();
   server.listen(PORT, () => {
-    console.log("Hello To Nasa Project");
+    console.log("Hello To Nasa Project, Let`s us Be CI/CD and Test again");
   });
 }
 
